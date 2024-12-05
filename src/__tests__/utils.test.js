cc6946ba9cf7f44b5bbbfed0c2751f23da7e6a42
Elvis-785 <kiruielvis3358@gmail.com>
2024-12-05 11:39:12 +0300

    writing unit tests in tdd

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for palindromes", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("returns false for non-palindromes", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  it("handles uppercase and lowercase input", () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  it("returns false for an empty string", () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  it("throws an error for non-alphabetic characters", () => {
+    expect(() => isPalindrome("racecar123")).toThrow();
+  });
+
+  it("throws an error for non-string input", () => {
+    expect(() => isPalindrome(123)).toThrow();
+  });
+});
